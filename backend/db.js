a130a74a2fab1fcabf7b862376378912d637b723
jose-calderon1996 <joseignaciocalderonroldan39@gmail.com>
2025-05-02 17:37:27 -0400

    aaa

--- backend/db.js
+++ backend/db.js
@@ -1,3 +1,16 @@
-const db = require('../db');
+// 📁 backend/db.js
+const mysql = require('mysql2/promise');
+require('dotenv').config();
 
-const [result] = await db.query('SELECT * FROM usuarios WHERE ...');
+const pool = mysql.createPool({
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
+});
+
+module.exports = pool;
