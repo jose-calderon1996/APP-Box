4339af986e89b6226e6388fa2e345ad938c88ad4
jose-calderon1996 <joseignaciocalderonroldan39@gmail.com>
2025-05-07 14:00:21 -0400

    a

---
+++ backend/firebase-key-json.js
@@ -0,0 +1,14 @@
+{
+    "type": "service_account",
+    "project_id": "mi-coach-personal",
+    "private_key_id": "8f13fec9fbc597a46fbf2dffcec9d3cfc09d109d",
+    "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQC+eErLBDznjig8\nGxTz0gC7w6sQ/jkQPEo45ydG+zWTwJ43BWQJ85Fzv92A7F3QbKWApD9rmi18J7Jt\n+WUns9NMSdvGAONn1yQESAD+Jg3gs7UjSaEonPvwitq9oJqLQscypxpn4n9CTrLq\nc7wvDd/VEIz+080rr4JBO4WUG0S1mw+cL4jP+3UMTe/jIjelLqaCliDDEFogOMYR\nqwiIDVGPUngT7iouQUf6sMTkLwIMl761WUITNS8fFDGiUlEJ3GHFvW1v4+Uh4psm\n6PWUttXDkP4XO4ea/YMBDl08YdXehDAoOTCz/Lnc2JJLMtKnR9FtkMRQ8JyRjWIL\nbfN93IKnAgMBAAECggEAD94OP7VzAXNVT2njfUsXETgEGJ6lJatwH3zE8o6GRcXR\nKGeZkD6rSlzx2LCtlr8bq94trmAiE+rBXD9fjvGAfnrUr+fDtL3hMT4dCbnP2X1U\nb254hN1McRs64ntDr1Wj1J4ZKWMJaP5iEf1hmvs+lLhpQT1nJTwyJLm5r2n0zRAg\na83mBKqDraDbXfB3qkuKB6jkUwwvRRwPO4Y4fVMtb05s+dmZ9cv8PndOx7CeCOKS\nlzvX9WYn6kxKbi7LZSv0+qfg3yTI93myUh8ejwKgVaRiBgo5NR8gfNS9aVAsYHwy\nbIGPMGXkWG0oxAGKWvxRCThiuR/OUkaTxNFzrkWoQQKBgQD9ZNx0bESG6awYDxsG\nTjXe+9zzXPcsewelciWk8pcqcwRpOtLwRNZ28Z7qHGSj1SL7cG5bn9izdqPyKPjT\nlJaemcs3VOPVNwTwD/IBpY997w/dYd1vZEosNcjSrx1vN7FPit/ZE+y4qV4MqkS5\nr4OyaLbYQiDpa7Ee73ORuVMCYQKBgQDAbcN/TUrJjg/QeeYjgL2XAIuiB++2mFwr\n4WUY5RReWtCdqPSJKtSUzvKxZJXZaw2Bfhon1Y7H0PDwyYUN7PbjMxh6xBlkklrL\nT6oZxI3LslXZVre8P2AA+RJjpZdI2JfhGPMzTbyZw5O5Svu1sbKVY6hE26FaLe4b\n+CzvuvCyBwKBgHov0qZJNtoff56yWgO2Xv/BT5ACeiX2xX8gBcr+N6p+f9gGR4UY\njJoadNjLDDR8KDw6wNxpveL8CJ5S53mPIvojivyJcLt1pmnkJnxoy0KcrnfAIzj9\nkU60OLxvOLBSmKuUedCBbRq2gOV2jWFEoyq6l8C8UoZ8KZKTQJU6Wi7BAoGACfBh\nbZzXZQuyY2k4atMGd9suWyMfzYWXFSkiKKQEZk84gTwtzTvqGPmLbYAtgTpXSz3x\nKeeM5WM0Xqt59NstcVS9SfIBAcHRcLjfp08Qp9nBN2WUPePkGIjE22K9KqUNSzIX\ntampOSPx6FsoHx1bhP3Nvii978N5jfZGk+dK8VcCgYEAqPXNw8FhV6H5r/rlEsS2\ntnowaHyGx1JtfB6c7k226FdQmI4Y43aPphQdUQotYkoWfeuccSAciNq5J6nhzne8\nvvYlz5RJTIT/9pFx0EM+WgjnSLxD20hrOqi0YILuvFum2eaOzBxKvzfwOAlGI5/g\nd78/sW6ahxxc5IYBjHL3GeE=\n-----END PRIVATE KEY-----\n",
+    "client_email": "firebase-adminsdk-fbsvc@mi-coach-personal.iam.gserviceaccount.com",
+    "client_id": "102817894882738350061",
+    "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+    "token_uri": "https://oauth2.googleapis.com/token",
+    "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+    "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-fbsvc%40mi-coach-personal.iam.gserviceaccount.com",
+    "universe_domain": "googleapis.com"
+  }
+  
--- backend/firebase.js
+++ backend/firebase.js
@@ -1,5 +1,5 @@
 const admin = require('firebase-admin');
-const serviceAccount = require('./firebase-key.json'); // ← Carga directa desde el archivo
+const serviceAccount = require('./firebase-key-json'); // ← Carga directa desde el archivo
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
