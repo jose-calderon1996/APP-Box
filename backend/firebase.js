4745d5f1130ccd6bfd2bb563d4b3887dfe62c52b
jose-calderon1996 <joseignaciocalderonroldan39@gmail.com>
2025-05-07 13:38:11 -0400

    A

--- backend/firebase.js
+++ backend/firebase.js
@@ -1,11 +1,9 @@
 const admin = require('firebase-admin');
 
-// Verificamos que la variable esté definida
 if (!process.env.FIREBASE_KEY_JSON) {
   throw new Error('🔥 La variable FIREBASE_KEY_JSON no está definida');
 }
 
-// ⚠️ IMPORTANTE: reemplazar \\n por \n para que Firebase lo entienda
 const serviceAccount = JSON.parse(
   process.env.FIREBASE_KEY_JSON.replace(/\\n/g, '\n')
 );
