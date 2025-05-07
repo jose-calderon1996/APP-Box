eb048cac76f0ede63a90b566dc8cec251b259a5d
jose-calderon1996 <joseignaciocalderonroldan39@gmail.com>
2025-05-06 23:39:15 -0400

    a

--- backend/firebase.js
+++ backend/firebase.js
@@ -1,22 +1,8 @@
-// Este archivo inicializa Firebase Admin SDK usando variables de entorno
-
 const admin = require('firebase-admin');
 
-// Creamos el objeto con las variables de entorno
-const serviceAccount = {
-  type: process.env.FB_TYPE,
-  project_id: process.env.FB_PROJECT_ID,
-  private_key_id: process.env.FB_PRIVATE_KEY_ID,
-  private_key: process.env.FB_PRIVATE_KEY.replace(/\\n/g, '\n'),
-  client_email: process.env.FB_CLIENT_EMAIL,
-  client_id: process.env.FB_CLIENT_ID,
-  auth_uri: process.env.FB_AUTH_URI,
-  token_uri: process.env.FB_TOKEN_URI,
-  auth_provider_x509_cert_url: process.env.FB_AUTH_PROVIDER,
-  client_x509_cert_url: process.env.FB_CLIENT_CERT
-};
+// Obtenemos el JSON completo desde la variable de entorno
+const serviceAccount = JSON.parse(process.env.FIREBASE_KEY_JSON.replace(/\\n/g, '\n'));
 
-// Inicializamos Firebase Admin
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
 });
