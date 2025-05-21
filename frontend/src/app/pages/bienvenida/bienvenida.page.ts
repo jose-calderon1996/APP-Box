1d05f0be8530898d68fcacf93966436fc6980790
jose-calderon1996 <joseignaciocalderonroldan39@gmail.com>
2025-05-21 17:59:31 -0400

    animaciones?

--- frontend/src/app/pages/bienvenida/bienvenida.page.ts
+++ frontend/src/app/pages/bienvenida/bienvenida.page.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { IonicModule } from '@ionic/angular';
+import { IonicModule, LoadingController, AlertController } from '@ionic/angular';
 import { RouterModule } from '@angular/router';
 import { FormsModule } from '@angular/forms';
 
@@ -11,4 +11,33 @@ import { FormsModule } from '@angular/forms';
   templateUrl: './bienvenida.page.html',
   styleUrls: ['./bienvenida.page.scss'],
 })
-export class BienvenidaPage { }
+export class BienvenidaPage {
+  constructor(
+    private loadingCtrl: LoadingController,
+    private alertCtrl: AlertController
+  ) {}
+
+  async realizarAccion() {
+    const loading = await this.loadingCtrl.create({
+      message: 'Cargando datos...',
+      spinner: 'circles',
+      cssClass: 'custom-loader',
+      backdropDismiss: false
+    });
+    await loading.present();
+
+    // Simula una acción
+    setTimeout(async () => {
+      await loading.dismiss();
+
+      const alert = await this.alertCtrl.create({
+        header: '✅ Acción Exitosa',
+        message: 'Inicio de sesion correcto...',
+        buttons: ['Aceptar'],
+        cssClass: 'custom-alert'
+      });
+
+      await alert.present();
+    }, 2000);
+  }
+}
